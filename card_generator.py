de612c81f716a63491240be482aa8613585a3667
deejes <j.devansh29@gmail.com>
2018-01-28 22:04:14 -0800

    add functions to generate numeric and visual set decks

--- card_generator.py
+++ card_generator.py
@@ -1,9 +1,5 @@
 # Each card has a number, color, shape, and fill.
 
-colors = ["red","green","purple"]
-count = [1,2,3]
-shape = ["circle", "triangle", "squiggly"]
-fill = ["solid","striped","empty"]
 
 
 # we could encode these all as numbers
@@ -13,16 +9,33 @@ fill = ["solid","striped","empty"]
 
 
 
-def return_set_deck():
-    range3 = [i for i in range(3)]
+def set_deck_creator():
     deck = []
     for a in range(3):
         for b in range(3):
             for c in range(3):
                 for d in range(3):
                     deck.append([a,b,c,d])
-
     # deck is a list containing 81 unique lists, each with 4 elements with values 0,1,2
     return deck
 
-print ((return_set_deck()))
+
+# just for fun, lets return each deck as it would appear graphically
+count = ["one","two","three"]
+fill = ["solid","striped","empty"]
+colors = ["red","green","purple"]
+shape = ["circle", "triangle", "squiggly"]
+card_attributes = [count,fill,colors,shape]
+
+def visual_deck_creator(deck):
+    visual_deck = deck
+    for card in visual_deck:
+        for j,feature in enumerate(card):
+           card[j] = card_attributes[j][feature]
+    return visual_deck
+
+visual_deck=visual_deck_creator(return_set_deck())
+
+
+
+
